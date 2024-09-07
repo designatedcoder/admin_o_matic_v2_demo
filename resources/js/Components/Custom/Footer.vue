7992cba055cd7bbbec65f99042307de09f26aa43
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2024-09-07 18:01:34 -0400

    admin layout to dashboard with smaller components

---
+++ resources/js/Components/Custom/Footer.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import { ref } from 'vue'
+
+    const currentDate = ref(new Date)
+</script>
+<template>
+    <footer class="main-footer">
+        <strong>Copyright &copy; 2014-{{ currentDate.getFullYear() }} <a href="https://adminlte.io">Admin-o-Matic</a>.</strong>
+        All rights reserved.
+        <div class="float-right d-none d-sm-inline-block">
+            <b>Version</b> 2.0
+        </div>
+    </footer>
+</template>
---
+++ resources/js/Layouts/AdminLayout.vue
@@ -0,0 +1,56 @@
+<script setup>
+    import { Head } from '@inertiajs/vue3'
+    import Navigation from '@/Components/Custom/Navigation.vue'
+    import Footer from '@/Components/Custom/Footer.vue'
+    import Sidebar from '@/Components/Custom/Sidebar.vue'
+
+    defineProps({
+        title: String,
+    });
+</script>
+
+<template>
+    <div class="wrapper">
+
+        <Head :title="title" />
+
+        <!-- Preloader -->
+        <!-- <div class="preloader flex-column justify-content-center align-items-center"> -->
+        <!--   <img class="animation__wobble" :src="'/storage/defaults/images/AdminLTELogo.png'" alt="AdminLTELogo" height="60" width="60"> -->
+        <!-- </div> -->
+
+        <!-- Navbar -->
+        <Navigation />
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <Sidebar />
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <slot name="header" />
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <slot name="content" />
+                </div><!--/. container-fluid -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        <!-- Main Footer -->
+        <Footer />
+    </div>
+    <!-- ./wrapper -->
+
+</template>
