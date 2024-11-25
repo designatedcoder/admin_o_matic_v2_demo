2366e44ae98c78749f583a2991d8ae655fbeb228
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2024-11-24 19:47:46 -0500

    created pagination component

---
+++ resources/js/Components/Custom/Pagination.vue
@@ -0,0 +1,26 @@
+<script setup>
+    import { Link } from '@inertiajs/vue3'
+    defineProps({
+        links: Array
+    });
+</script>
+
+<template>
+    <nav v-if="links.length > 4">
+        <ul class="pagination pagination-sm sm-0 float-right">
+            <li class="page-item"
+                :class="[
+                link.url === null ? 'disabled' : '',
+                link.active ? 'active' : ''
+                ]"
+                v-for="(link, index) in links"
+                :key="index"
+            >
+                <Link class="page-link"
+                    :href="link.url === null ? '#' : link.url"
+                    v-html="link.label"
+                ></Link>
+            </li>
+        </ul>
+    </nav>
+</template>
